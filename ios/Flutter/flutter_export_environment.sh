7a13a6984f9f66a218d2a5d39fa9d891bc74bf7b
CoderSamz <codersamz-dev@outlook.com>
2020-01-12 02:08:18 +0800

    1.更换iOSApp icon 2.移除iOS推送通知证书并关闭推送通知

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/xj.deng/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/xj.deng/alibaba/xj_fluttergo/flutter-go"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=/Users/Samz/Documents/Development/tools/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/Samz/Desktop/CoderTseLaboratory/FlutterProject/flutter-go"
+export "FLUTTER_TARGET=/Users/Samz/Desktop/CoderTseLaboratory/FlutterProject/flutter-go/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/xj.deng/development/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_FRAMEWORK_DIR=/Users/Samz/Documents/Development/tools/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.6"
 export "FLUTTER_BUILD_NUMBER=1.0.6"
+export "TRACK_WIDGET_CREATION=true"
